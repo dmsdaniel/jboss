98b358a2ad2ffc263ac6f996e5ef8f1c7aa927b9
daniel santos <daniel@wareline.com.br>
2017-06-06 16:28:58 -0300

    My commit

--- src/main/java/br/com/daniel/repository/PessoaRepository.java
+++ src/main/java/br/com/daniel/repository/PessoaRepository.java
@@ -6,43 +6,45 @@
 package br.com.daniel.repository;
 
 import java.time.LocalDateTime;
- 
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
- 
+
 import br.com.daniel.model.PessoaModel;
 import br.com.daniel.repository.entity.PessoaEntity;
 import br.com.daniel.repository.entity.UsuarioEntity;
 import br.com.daniel.uteis.Uteis;
- 
+
 public class PessoaRepository {
- 
-	@Inject
-	PessoaEntity pessoaEntity;
- 
-	EntityManager entityManager;
- 
-	/***
-	 * MÉTODO RESPONSÁVEL POR SALVAR UMA NOVA PESSOA
-	 * @param pessoaModel
-	 */
-	public void SalvarNovoRegistro(PessoaModel pessoaModel){
- 
-		entityManager =  Uteis.JpaEntityManager();
- 
-		pessoaEntity = new PessoaEntity();
-		pessoaEntity.setDataCadastro(LocalDateTime.now());
-		pessoaEntity.setEmail(pessoaModel.getEmail());
-		pessoaEntity.setEndereco(pessoaModel.getEndereco());
-		pessoaEntity.setNome(pessoaModel.getNome());
-		pessoaEntity.setOrigemCadastro(pessoaModel.getOrigemCadastro());
-		pessoaEntity.setSexo(pessoaModel.getSexo());
- 
-		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class, pessoaModel.getUsuarioModel().getCodigo()); 
- 
-		pessoaEntity.setUsuarioEntity(usuarioEntity);
- 
-		entityManager.persist(pessoaEntity);
- 
-	}
+
+    @Inject
+    PessoaEntity pessoaEntity;
+
+    EntityManager entityManager;
+
+    /**
+     * *
+     * MÉTODO RESPONSÁVEL POR SALVAR UMA NOVA PESSOA
+     *
+     * @param pessoaModel
+     */
+    public void SalvarNovoRegistro(PessoaModel pessoaModel) {
+
+        entityManager = Uteis.JpaEntityManager();
+
+        pessoaEntity = new PessoaEntity();
+        pessoaEntity.setDataCadastro(LocalDateTime.now());
+        pessoaEntity.setEmail(pessoaModel.getEmail());
+        pessoaEntity.setEndereco(pessoaModel.getEndereco());
+        pessoaEntity.setNome(pessoaModel.getNome());
+        pessoaEntity.setOrigemCadastro(pessoaModel.getOrigemCadastro());
+        pessoaEntity.setSexo(pessoaModel.getSexo());
+
+        UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class, pessoaModel.getUsuarioModel().getCodigo());
+
+        pessoaEntity.setUsuarioEntity(usuarioEntity);
+
+        entityManager.persist(pessoaEntity);
+
+    }
 }
--- src/main/java/br/com/daniel/uteis/Uteis.java
+++ src/main/java/br/com/daniel/uteis/Uteis.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.daniel.uteis;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
@@ -16,7 +12,8 @@ public class Uteis {
 
         FacesContext facesContext = FacesContext.getCurrentInstance();
 
-        ExternalContext externalContext = facesContext.getExternalContext();
+        ExternalContext externalContext;
+        externalContext = facesContext.getExternalContext();
 
         HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
 
